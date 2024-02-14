bf2f57115aa5e6fa213ec298ca07c360fd4ca80e
Yasin-Danur <yasin.denur.awal@gmail.com>
2024-02-14 12:51:29 +0300

    the carousel effect

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CarouselEffect from "./Components/Carousel/CarouselEffect";
 import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CarouselEffect />
     </div>
   );
 }
